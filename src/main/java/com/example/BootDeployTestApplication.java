64dfbe7be65a80521bdb1aff8f076b583d11507c
mekappi <mekappi@gmail.com>
2018-06-22 12:23:52 +0900

    firstcommit

---
+++ src/main/java/com/example/BootDeployTestApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootDeployTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootDeployTestApplication.class, args);
+	}
+}
